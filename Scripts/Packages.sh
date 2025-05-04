ad9f845998177c495bbf08893cb0a9c695fe3d32
AltarsCN <108567979+AltarsCN@users.noreply.github.com>
2025-05-05 01:27:09 +0800

    quectel-CM-5G

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -63,6 +63,7 @@ UPDATE_PACKAGE "mosdns" "sbwml/luci-app-mosdns" "v5" "" "v2dat"
 UPDATE_PACKAGE "qmodem" "FUjr/modem_feeds" "main"
 UPDATE_PACKAGE "viking" "VIKINGYFY/packages" "main" "" "luci-app-timewol luci-app-wolplus"
 UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
+UPDATE_PACKAGE "quectel-CM-5G" "QMODEM/quectel-CM-5G" "main"
 
 if [[ $WRT_REPO != *"immortalwrt"* ]]; then
 	UPDATE_PACKAGE "qmi-wwan" "immortalwrt/wwan-packages" "master" "pkg"
